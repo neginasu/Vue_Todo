621c85f2c76ba8a949683111794efeb27153a070
neginasu <neginasu7@gmail.com>
2024-02-26 01:08:39 +0900

    feat: buttonクリック時の機能を追加

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -1,12 +1,15 @@
 <script setup>
   import { ref } from 'vue';
   const todoRef = ref('');
+  const addTodo = () => {
+  console.log(todoRef.value);
+  };
 </script>
 
 <template>
   <div class="box_input">
     <input type="text" class="todo_input" v-model="todoRef" placeholder="TODOを入力" />
-    <button class="btn">追加</button>
+    <button class="btn" @click="addTodo">追加</button>
   </div>
 </template>
 
